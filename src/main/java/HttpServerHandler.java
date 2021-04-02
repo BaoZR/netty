57e45fe976521fef2b6ea73d80fd91de6924aa4b
BaoZhengRan <474234165@qq.com>
2021-04-02 17:34:35 +0800

    netty，简单的HTTP服务器，来自若地的“引导器作用：客户端和服务端启动都要做些什么？”

---
+++ src/main/java/HttpServerHandler.java
@@ -0,0 +1,24 @@
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+/*
+ * @Author  :bob
+ * @Date    :Created in 17:12 2021/4/2
+ * @Description:
+ *
+ */
+public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest msg) {
+        String content = String.format("Receive http request, uri: %s, method: %s, content: %s%n", msg.uri(), msg.method(), msg.content().toString(CharsetUtil.UTF_8));
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.wrappedBuffer(content.getBytes()));
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+}
